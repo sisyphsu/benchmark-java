dfbdf2453a1ef55cb466198a28691f18ba1ac04f
sulin <sulinixl@163.com>
2019-10-22 15:51:36 +0800

    backup

--- benchmark/src/main/java/com/github/sisyphsu/benchmark/utils/IdentityMapBenchmark.java
+++ benchmark/src/main/java/com/github/sisyphsu/benchmark/utils/IdentityMapBenchmark.java
@@ -7,10 +7,19 @@ import java.util.IdentityHashMap;
 import java.util.concurrent.TimeUnit;
 
 /**
+ * 数据集4096
  * Benchmark                      Mode  Cnt  Score   Error  Units
  * IdentityMapBenchmark.contains  avgt    9  1.937 ± 0.016  ns/op
  * IdentityMapBenchmark.get       avgt    9  2.061 ± 0.009  ns/op
  * IdentityMapBenchmark.put       avgt    9  2.988 ± 0.369  ns/op
+ * <p>
+ * 数据集256
+ * Benchmark                      Mode  Cnt  Score   Error  Units
+ * IdentityMapBenchmark.contains  avgt    9  1.946 ± 0.014  ns/op
+ * IdentityMapBenchmark.get       avgt    9  2.070 ± 0.010  ns/op
+ * IdentityMapBenchmark.put       avgt    9  2.957 ± 0.059  ns/op
+ * <p>
+ * 性能表现与数据量没有关系，比较典型的O(1)时间复杂度
  *
  * @author sulin
  * @since 2019-10-22 11:28:36
@@ -23,7 +32,7 @@ import java.util.concurrent.TimeUnit;
 public class IdentityMapBenchmark {
 
     private static final IdentityHashMap<Object, Object> map  = new IdentityHashMap<>();
-    private static final Object[]                        keys = new Object[1 << 12];
+    private static final Object[]                        keys = new Object[1 << 8];
 
     static {
         for (int i = 0; i < keys.length; i++) {
@@ -35,17 +44,17 @@ public class IdentityMapBenchmark {
 
     @Benchmark
     public void contains() {
-        map.containsKey(keys[1024]);
+        map.containsKey(keys[64]);
     }
 
     @Benchmark
     public void get() {
-        map.get(keys[1024]);
+        map.get(keys[64]);
     }
 
     @Benchmark
     public void put() {
-        map.put(keys[1024], keys[1024]);
+        map.put(keys[64], keys[64]);
     }
 
     public static void main(String[] args) {
